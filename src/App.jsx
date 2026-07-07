fd91eb8a372e93fae21c95b2387b45fc020730ee
ujaasagarwal <ujaas1234@gmail.com>
2026-07-08 01:25:36 +0530

    added basic preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Land from "./pages/Landing";
 import ScrollToTop from "./components/ScrollToTop";
 import Blog from "./pages/Blog";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#000",
+          color: "#fff",
+          fontSize: "2rem",
+          fontWeight: "bold",
+        }}
+      >
+        Loading...
+      </div>
+    );
+  }
+
   return (
     <>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Land />} />
         <Route path="/blog" element={<Blog />} />
-      
       </Routes>
-      
     </>
   );
 };
