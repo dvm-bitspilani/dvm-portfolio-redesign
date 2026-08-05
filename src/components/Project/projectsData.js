dae5c7c1a2bff54fc75875d689a4ad24e56f6544
Gantavya <gantavyamishra@gmail.com>
2026-08-05 18:19:22 +0530

    Projects data updated

--- src/components/Project/projectsData.js
+++ src/components/Project/projectsData.js
@@ -1,408 +1,63 @@
-const projects = [
-  {
-    "id": "armageddon-20-website",
-    "name": "Armageddon 20 Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "March 2020",
-    "websiteLink": "https://bits-apogee.org/",
-    "heroImage": "./assets/img/project-imgs/hero/armageddon-hero-min.png",
-    "text1": "APOGEE(A Professions Oriented Gathering over Educational Experiences)- is BITS Pilani's annual technical extravaganza. From presenting papers and projects at the oldest-of-its-kind events in the country, developing amazing solutions to real-life cases and problems, exhibitions of the best contraptions that man has made, guest lectures that tell stories never heard before, to the college's own literature festival, APOGEE challenges the intellect of the participants and piques the minds of the audience.",
-    "text2": "Armageddon is the gaming event in APOGEE. This event brings together many gaming enthusiasts from colleges all over the country. It attracts a massive participation pool of around 1.5k-2k gamers every year. Games hosted during Armageddon are CS: GO, FIFA, Rainbow 6 Siege, PUBG Mobile, and Mini Militia.",
-    "gallery": [
-      "./assets/img/project-imgs/long/armageddon-min.png",
-      "./assets/img/project-imgs/mobile-long/armageddon-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/armageddon-mockup-min.png"
-  },
-  {
-    "id": "bosm-19-website",
-    "name": "BOSM 19 Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "Sept 2019",
-    "websiteLink": "http://bits-bosm.org/",
-    "heroImage": "./assets/img/project-imgs/hero/bosm19-hero-min.png",
-    "text1": "The BITS Open Sports Meet (BOSM) is the annual sports festival of BITS Pilani, Pilani Campus. This much awaited sporting extravaganza is a platform for college teams to exhibit their exceptional sporting talents and compete for superiority! Since its first edition way back in 1986, it has grown to be one of the largest and most competitive college sports fests in the country.",
-    "text2": "This was the Official Website for BOSM'19. It was aimed to ease the Registration process for the people participating in the fest. It has many other sections like the details of all the sports events, gallery, and a note from the Director of BITS Pilani.",
-    "gallery": [
-      "./assets/img/project-imgs/long/bosm19-min.png",
-      "./assets/img/project-imgs/mobile-long/bosm19-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/bosm19-mockup-min.png"
-  },
-  {
-    "id": "rocktaves-website",
-    "name": "Rocktaves Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "Oct 2019",
-    "websiteLink": "https://bits-oasis.org/rocktaves/",
-    "heroImage": "./assets/img/project-imgs/hero/rocktaves-hero-min.png",
-    "text1": "Oasis—the cultural fest of BITS Pilani—is nothing but 96 hours of unadulterated thrills comprising music, dance, drama, art, quizzes, fashion, and humour. With a footfall of around 5000+ every year, it is the second-largest cultural festival of India.",
-    "text2": "Rocktaves is India's one of the oldest band competitions, which has acted as a springboard for India's premier rock groups of the stature of Parikrama, Indian Ocean, Superfuzz, PRESTORIKA, Them Clones, and Euphoria, who have taken part in the competition at different points in its history.",
-    "gallery": [
-      "./assets/img/project-imgs/long/rocktaves-min.png",
-      "./assets/img/project-imgs/mobile-long/rocktave-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/rocktave-mockup-min.png"
-  },
-  {
-    "id": "oasis-20-website",
-    "name": "Oasis 20 Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "Oct 2020",
-    "websiteLink": "http://bits-oasis.org/",
-    "heroImage": "./assets/img/project-imgs/hero/oasis-hero-min.png",
-    "text1": "Oasis—the cultural fest of BITS Pilani—is nothing but 96 hours of unadulterated thrills comprising music, dance, drama, art, quizzes, fashion, and humour. With a footfall of around 5000+ every year, it is the second-largest cultural festival of India.",
-    "text2": "This was the Official Website for Oasis'20. It eased the Registrations for all the events, some of which were paid. It shows the details and rules for all the events along with their timelines. It had many miscellaneous sections, too, like Videos, Sponsors' Page, Gallery, among others. Users could also Login through the website itself.",
-    "gallery": [
-      "./assets/img/project-imgs/long/oasis-min.png",
-      "./assets/img/project-imgs/mobile-long/oasis-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/oasis-mockup-min.png"
-  },
-  {
-    "id": "armageddon-19-website",
-    "name": "Armageddon 19 Website",
-    "categories": [
-      "Frontend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "March 2019",
-    "websiteLink": null,
-    "heroImage": "./assets/img/project-imgs/hero/armageddon19-hero-min.png",
-    "text1": null,
-    "text2": "Armageddon is the gaming event in APOGEE. This event brings together many gaming enthusiasts from colleges all over the country. It attracts a massive participation pool of around 1.5k-2k gamers every year. Games hosted during Armageddon are CS: GO, FIFA, Rainbow 6 Siege, PUBG Mobile, and Mini Militia.",
-    "gallery": [
-      "./assets/img/project-imgs/hero/armageddon19-hero-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/armageddon19-mockup-min.png"
-  },
-  {
-    "id": "profshows-website",
-    "name": "Profshows website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "Oct 2019",
-    "websiteLink": "https://bits-oasis.org/profshows/",
-    "heroImage": "./assets/img/project-imgs/hero/profshows-hero-min.png",
-    "text1": "Oasis—the cultural fest of BITS Pilani—is nothing but 96 hours of unadulterated thrills comprising music, dance, drama, art, quizzes, fashion, and humour. With a footfall of around 5000+ every year, it is the second-largest cultural festival of India.",
-    "text2": "ProfShows is the term given to a collection of significant Concerts and Events held during Oasis. In 2019, celebrities like- Nucleya, Biswa Kalyan Rath, Sunidhi Chauhan performed during the fest. Many other famous personalities like Zakir Khan, Abhishek Upmanyu, and Shankar Ehsan Loy have performed during Oasis in past years.",
-    "gallery": [
-      "./assets/img/project-imgs/long/profshows-min.png",
-      "./assets/img/project-imgs/mobile-long/profshows-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/profshows-mockup-min.png"
-  },
-  {
-    "id": "apogee-innovation-challenge-19",
-    "name": "Apogee Innovation Challenge 19",
-    "categories": [
-      "Frontend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "March 2019",
-    "websiteLink": null,
-    "heroImage": "./assets/img/project-imgs/hero/aic-hero-min.png",
-    "text1": null,
-    "text2": null,
-    "gallery": [
-      "./assets/img/project-imgs/long/aic-min.png",
-      "./assets/img/project-imgs/mobile-long/aic-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/aic-mockup-min.png"
-  },
-  {
-    "id": "oasis-19-app",
-    "name": "Oasis 19 App",
-    "categories": [
-      "AppDev",
-      "Backend",
-      "Design"
-    ],
-    "type": "app",
-    "date": "Oct 2019",
-    "websiteLink": "https://tinyurl.com/oasis19-app",
-    "heroImage": "./assets/img/project-imgs/hero/oasisapp-hero-min.png",
-    "text1": "Oasis—the cultural fest of BITS Pilani—is nothing but 96 hours of unadulterated thrills comprising music, dance, drama, art, quizzes, fashion, and humour. With a footfall of around 5000+ every year, it is the second-largest cultural festival of India.",
-    "text2": "This app was aimed to ease participation of students in the fest, comprising of many features such as events tracking, food ordering, QR based sign-ups and many more.",
-    "gallery": [
-      "./assets/img/project-imgs/long/oasisapp-min.png",
-      "./assets/img/project-imgs/mobile-long/oasisapp-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/oasisapp-mockup-min.png"
-  },
-  {
-    "id": "apogee-21-website",
-    "name": "Apogee 21 Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "March 2021",
-    "websiteLink": "http://www.bits-apogee.org/",
-    "heroImage": "./assets/img/project-imgs/hero/apogee21-hero-min.png",
-    "text1": "APOGEE(A Professions Oriented Gathering over Educational Experiences)- is BITS Pilani's annual technical extravaganza. From presenting papers and projects at the oldest-of-its-kind events in the country, developing amazing solutions to real-life cases and problems, exhibitions of the best contraptions that man has made, guest lectures that tell stories never heard before, to the college's own literature festival, APOGEE challenges the intellect of the participants and piques the minds of the audience.",
-    "text2": "This was the Official Website of APOGEE'21, based on the theme- \"Digitized Renaissance.\" It eased the Registrations for all the events, some of which were paid. It shows the details and rules for all the events along with their timelines. It had many miscellaneous sections and pages like- Videos, Sponsors' Page, WallMag, among many others. This time there was a Kind Store Page where all the participants could redeem their kind points and get various discount coupons.",
-    "gallery": [
-      "./assets/img/project-imgs/long/apogee21-min.png",
-      "./assets/img/project-imgs/mobile-long/apogee21-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/apogee21-mockup-min.png"
-  },
-  {
-    "id": "bosm-19-app",
-    "name": "BOSM 19 App",
-    "categories": [
-      "AppDev",
-      "Backend",
-      "Design"
-    ],
-    "type": "app",
-    "date": "Sept 2019",
-    "websiteLink": "https://tinyurl.com/bosm19-app",
-    "heroImage": "./assets/img/project-imgs/hero/bosmapp-hero-min.png",
-    "text1": "The BITS Open Sports Meet (BOSM) is the annual sports festival of BITS Pilani, Pilani Campus. This much-awaited sporting extravaganza is a platform for college teams to exhibit their exceptional sporting talents and compete for superiority! Since its first edition way back in 1986, it has grown to be one of the largest and most competitive college sports fests in the country.",
-    "text2": "This app allowed users to keep track of the sports events and competitions and notify accordingly. Users were able to order food and pay using the wallet feature of the app. Users could also play games and quiz. Apart from all these major features, there were many miscellaneous features too.",
-    "gallery": [
-      "./assets/img/project-imgs/long/bosmapp-min.png",
-      "./assets/img/project-imgs/mobile-long/bosmapp-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/bosmapp-mockup-min.png"
-  },
-  {
-    "id": "apogee-21-curtain-raiser",
-    "name": "Apogee '21 Curtain Raiser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://EykvH3KETnc.com",
-    "youtubeId": "EykvH3KETnc"
-  },
-  {
-    "id": "apogee-21-theme-release",
-    "name": "Apogee '21 Theme Release",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://z-Pt8nBEa9E.com",
-    "youtubeId": "z-Pt8nBEa9E"
-  },
-  {
-    "id": "apogee-20-theme-releaser",
-    "name": "Apogee '20 Theme Releaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://Uvo2vI9Cco.com",
-    "youtubeId": "Uvo2_vI9Cco"
-  },
-  {
-    "id": "apogee-20-promo",
-    "name": "Apogee '20 Promo",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://DXPVKwoMB5E.com",
-    "youtubeId": "DXPVKwoMB5E"
-  },
-  {
-    "id": "oasis-20-theme-release",
-    "name": "Oasis '20 Theme Release",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://C8Mg71oIA2s.com",
-    "youtubeId": "C8Mg71oIA2s"
-  },
-  {
-    "id": "50-years-of-oasis",
-    "name": "50 Years of Oasis",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": "http://53VKUrM5HY.com",
-    "youtubeId": "53_VKUrM5HY"
-  },
-  {
-    "id": "apogee-19-theme-releaser",
-    "name": "Apogee '19 Theme Releaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "MqpPYIjDw4I"
-  },
-  {
-    "id": "oasis-19-teaser",
-    "name": "Oasis '19 Teaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "35lWrDmkxko"
-  },
-  {
-    "id": "oasis-19-rocktaves-promo",
-    "name": "Oasis '19 Rocktaves Promo",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "0AlBafi2NiQ"
-  },
-  {
-    "id": "oasis-19-promo",
-    "name": "Oasis '19 Promo",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "ennOr12w52k"
-  },
-  {
-    "id": "oasis-19-nucleya-teaser",
-    "name": "Oasis '19 Nucleya Teaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "wUyg7-x5jcs"
-  },
-  {
-    "id": "oasis-19-fashp-promo",
-    "name": "Oasis '19 Fashp Promo",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "T9KZB_zipmw"
-  },
-  {
-    "id": "oasis-19-curtain-raiser",
-    "name": "Oasis '19 Curtain Raiser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "wl3XLIzR4QQ"
-  },
-  {
-    "id": "oasis-19-biswa-teaser",
-    "name": "Oasis '19 Biswa Teaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "C2B798UjYUM"
-  },
-  {
-    "id": "oasis-18-teaser",
-    "name": "Oasis '18 Teaser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "oWDZY6y7ED8"
-  },
-  {
-    "id": "oasis-18-promo",
-    "name": "Oasis '18 Promo",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "b54gY5egToU"
-  },
-  {
-    "id": "oasis-18-curtain-raiser",
-    "name": "Oasis '18 Curtain Raiser",
-    "categories": [
-      "Video"
-    ],
-    "type": "video",
-    "date": null,
-    "websiteLink": null,
-    "youtubeId": "W5RB3xbZiJA"
-  },
-  {
-    "id": "aarohan-21-website",
-    "name": "Aarohan 21 Website",
-    "categories": [
-      "Frontend",
-      "Backend",
-      "Design"
-    ],
-    "type": "website",
-    "date": "March 2021",
-    "websiteLink": "https://dvm-bitspilani.github.io/Aarohan-2020/",
-    "heroImage": "./assets/img/project-imgs/hero/aarohan-hero-min.png",
-    "text1": "APOGEE(A Professions Oriented Gathering over Educational Experiences)- is BITS Pilani's annual technical extravaganza. From presenting papers and projects at the oldest-of-its-kind events in the country, developing amazing solutions to real-life cases and problems, exhibitions of the best contraptions that man has made, guest lectures that tell stories never heard before, to the college's own literature festival, APOGEE challenges the intellect of the participants and piques the minds of the audience.",
-    "text2": "Aarohan is a national-level examination crafted and conducted by the students of BITS Pilani. The examination is conducted for the classes of IX, X, XI, and XIIth. It is conducted in two rounds. The winners of round 1 are then invited to BITS Pilani to be a part of round 2. The winners of round 2 win cash prizes and much more.",
-    "gallery": [
-      "./assets/img/project-imgs/long/aarohan-min.png",
-      "./assets/img/project-imgs/mobile-long/aarohan-mobile-min.png"
-    ],
-    "mockup": "./assets/img/project-imgs/mockup/aarohan-mockup-min.png"
-  }
-];
- 
+import rawProjects from "./projects.json";
+
+const projects = rawProjects.map((item) => {
+  const f = item.fields;
+
+  const categories = f.teamsInvolved
+    .split(",")
+    .map((x) => x.trim())
+    .map((x) => {
+      switch (x) {
+        case "Front":
+          return "Frontend";
+        case "Back":
+          return "Backend";
+        case "App":
+          return "AppDev";
+        default:
+          return x;
+      }
+    });
+
+  return {
+    id: String(item.pk),
+
+    name: f.name,
+
+    categories,
+
+    type: categories.includes("Video") ? "video" : "website",
+
+    date: f.date === "-" ? null : f.date,
+
+    websiteLink:
+      f.website_link === "http://null.com"
+        ? null
+        : f.website_link,
+
+    heroImage:
+      f.heroSectionImageLink === "-"
+        ? null
+        : f.heroSectionImageLink,
+
+    text1: f.text_1 === "-" ? null : f.text_1,
+
+    text2: f.text_2 === "-" ? null : f.text_2,
+
+    gallery:
+      f.long_images_link === "-"
+        ? []
+        : f.long_images_link.split(",").map((img) => img.trim()),
+
+    mockup:
+      f.mockups_link === "-"
+        ? null
+        : f.mockups_link,
+
+    youtubeId: categories.includes("Video")
+      ? f.heroSectionImageLink
+      : null,
+  };
+});
+
 export default projects;
